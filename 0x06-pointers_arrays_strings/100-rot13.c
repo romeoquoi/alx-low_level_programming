b4bdd278b250e4743cd8b8f12dba93790e397fdc
romeoquoi <romeoquoi@gmail.com>
2023-03-30 07:15:17 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,29 @@
 #include "main.h"
-
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  *
-  * Return: Nothing
-  */
-void print_number(int n)
+ * rot13 - rot13 encoding
+ * Return: pointer to arr
+ * @s: string
+ */
+char *rot13(char *s)
 {
-	unsigned int x;
+	char half1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 
-	if (n < 0)
+	char half2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i = 0, j;
+
+	while (s[i] != 0)
 	{
-		_putchar('-');
-		n *= -1;
+		char c = s[i];
+
+		for (j = 0; j < 52; j++)
+		{
+			if (c == half1[j])
+			{
+				s[i] = half2[j];
+			}
+		}
+		i++;
 	}
-
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	return (s);
 }
